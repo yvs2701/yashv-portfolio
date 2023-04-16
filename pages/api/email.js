bacf364019571cd614f1f609770267561603533c
yvs2701 <yashvardhan.ys86@gmail.com>
2023-04-16 15:06:22 +0530

    minor improvements in buggy email sender Uses Mailgun instead of SendInBlue due to profile getting blocked

--- pages/api/email.js
+++ pages/api/email.js
@@ -1,5 +1,6 @@
-import jwt from "jsonwebtoken"
+import verify from "jsonwebtoken/verify" 
 import nodemailer from "nodemailer"
+import { template } from "./templates/template"
 
 export default function handler(req, res) {
   try {
@@ -20,7 +21,7 @@ export default function handler(req, res) {
     }
 
     return new Promise((resolve, reject) => {
-      jwt.verify(answer, process.env.JWT_SECRET, async (err, decoded) => {
+      verify(answer, process.env.JWT_SECRET, async (err, decoded) => {
         if (err) {
           console.error(err);
           if (err.name === 'TokenExpiredError') {
@@ -44,80 +45,15 @@ export default function handler(req, res) {
 
             const info = await transporter.sendMail({
               from: `"${process.env.MAIL_USERNAME}" <${process.env.MAIL_USER}>`,
-              to: process.env.MAIL_USER,
-              replyTo: process.env.MAIL_USER,
+              to: process.env.MAIL_TO,
+              cc: process.env.MAIL_CC,
               subject: `${name}'s message received!`,
               text: `${name} <${email}> said: ${message}`,
-              html: `
-                  <tbody>
-                  <tr>
-                    <td class="drow" style="background-color: #ffffff; box-sizing: border-box; font-size: 0px; text-align: center;" valign="top" align="center">
-                      <div class="layer_2" style="max-width: 596px; display: inline-block; vertical-align: top; width: 100%;">
-                        <table class="edcontent" style="border-collapse: collapse;width:100%" cellspacing="0" border="0">
-                          <tbody>
-                            <tr>
-                              <td class="edtext" style="padding: 10px 20px; text-align: left; color: #5f5f5f; font-size: 16px; font-family: &quot;Open Sans&quot;, &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif; word-break: break-word; direction: ltr; box-sizing: border-box;" valign="top">
-                                <p class="style1 text-center" style="text-align: center; margin: 0px; padding: 0px; color: #000000; font-size: 32px; font-family: &quot;Open Sans&quot;, &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif;">Message received!</p>
-                              </td>
-                            </tr>
-                          </tbody>
-                        </table>
-                      </div>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td class="drow" style="background-color: #ffffff; box-sizing: border-box; font-size: 0px; text-align: center;" valign="top" align="center">
-                      <div class="layer_2" style="display: inline-block; vertical-align: top; width: 100%; max-width: 600px;">
-                        <table class="edcontent" style="border-collapse: collapse;width:100%" cellspacing="0" border="0">
-                          <tbody>
-                            <tr>
-                              <td class="edtext" style="padding: 10px 20px; text-align: left; color: #5f5f5f; font-size: 16px; font-family: &quot;Open Sans&quot;, &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif; word-break: break-word; direction: ltr; box-sizing: border-box;" valign="top">
-                                <p class="style2" style="margin: 0px; padding: 0px; color: #000000; font-size: 22px; font-family: &quot;Open Sans&quot;, &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif;"><strong>Name:</strong></p>
-                                <p style="margin: 0px; padding: 0px;">${name}</p>
-                              </td>
-                            </tr>
-                          </tbody>
-                        </table>
-                      </div>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td class="drow" style="background-color: #ffffff; box-sizing: border-box; font-size: 0px; text-align: center;" valign="top" align="center">
-                      <div class="layer_2" style="display: inline-block; vertical-align: top; width: 100%; max-width: 600px;">
-                        <table class="edcontent" style="border-collapse: collapse;width:100%" cellspacing="0" border="0">
-                          <tbody>
-                            <tr>
-                              <td class="edtext" style="padding: 10px 20px; text-align: left; color: #5f5f5f; font-size: 16px; font-family: &quot;Open Sans&quot;, &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif; word-break: break-word; direction: ltr; box-sizing: border-box;" valign="top">
-                                <p class="style2" style="margin: 0px; padding: 0px; color: #000000; font-size: 22px; font-family: &quot;Open Sans&quot;, &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif;"><strong>Email:</strong></p>
-                                <p style="margin: 0px; padding: 0px;">${email}</p>
-                              </td>
-                            </tr>
-                          </tbody>
-                        </table>
-                      </div>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td class="drow" style="background-color: #ffffff; box-sizing: border-box; font-size: 0px; text-align: center;" valign="top" align="center">
-                      <div class="layer_2" style="display: inline-block; vertical-align: top; width: 100%; max-width: 600px;">
-                        <table class="edcontent" style="border-collapse: collapse;width:100%" cellspacing="0" border="0">
-                          <tbody>
-                            <tr>
-                              <td class="edtext" style="padding: 10px 20px; text-align: left; color: #5f5f5f; font-size: 16px; font-family: &quot;Open Sans&quot;, &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif; word-break: break-word; direction: ltr; box-sizing: border-box;" valign="top">
-                                <p class="style2" style="margin: 0px; padding: 0px; color: #000000; font-size: 22px; font-family: &quot;Open Sans&quot;, &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif;"><strong>Message:</strong></p>
-                                <pre style="margin: 0px; padding: 0px; font-family: &quot;Open Sans&quot;, &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif;">${message}</pre>
-                              </td>
-                            </tr>
-                          </tbody>
-                        </table>
-                      </div>
-                    </td>
-                  </tr>
-                </tbody>`,
+              html: template(name, email, message),
               disableFileAccess: true,
             })
 
-            console.log(info)
+            console.log('SMTP response:', info)
             res.status(200).json({ success: true, data: "Thank you for your message, I will get back to you soon!" })
             resolve()
           } catch (err) {
