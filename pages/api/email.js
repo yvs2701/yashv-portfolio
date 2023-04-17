6684825d659faf67b07c6e51847e8f4623386fe6
yvs2701 <yashvardhan.ys86@gmail.com>
2023-04-17 22:20:43 +0530

    finally email service up and running

--- pages/api/email.js
+++ pages/api/email.js
@@ -1,6 +1,5 @@
-import verify from "jsonwebtoken/verify" 
+import verify from "jsonwebtoken/verify"
 import nodemailer from "nodemailer"
-import { template } from "./templates/template"
 
 export default function handler(req, res) {
   try {
@@ -31,8 +30,9 @@ export default function handler(req, res) {
           }
           resolve()
         }
-        else if (captcha === decoded.token) {
+        else if (captcha === decoded.token && typeof name === 'string' && typeof email === 'string' && typeof message === 'string') {
           try {
+            
             const transporter = nodemailer.createTransport({
               host: process.env.MAIL_SMTP,
               port: process.env.MAIL_PORT,
@@ -46,21 +46,25 @@ export default function handler(req, res) {
             const info = await transporter.sendMail({
               from: `"${process.env.MAIL_USERNAME}" <${process.env.MAIL_USER}>`,
               to: process.env.MAIL_TO,
-              cc: process.env.MAIL_CC,
+              replyTo: process.env.MAIL_USER,
               subject: `${name}'s message received!`,
               text: `${name} <${email}> said: ${message}`,
-              html: template(name, email, message),
+              html: `<head><link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,600,600i,700,700i &subset=cyrillic,latin-ext" data-name="open_sans" rel="stylesheet" type="text/css"></head><body style="padding:0;margin:0;background:#e4e6ec"><table style="height:100%;width:100%;background-color:#e4e6ec" align="center"><tbody><tr><td id="dbody" data-version="2.31" style="width:100%;height:100%;padding-top:50px;padding-bottom:50px;background-color:#e4e6ec" valign="top"><table style="max-width:600px;box-sizing:border-box;width:100%;margin:0 auto" cellspacing="0" cellpadding="0" border="0" align="center"><tbody><tr><td class="drow" style="background-color:#fff;box-sizing:border-box;font-size:0;text-align:center" valign="top" align="center"><div style="max-width:596px;display:inline-block;vertical-align:top;width:100%"><table class="edcontent" style="border-collapse:collapse;width:100%" cellspacing="0" border="0"><tbody><tr><td class="edtext" style="padding:10px 20px;text-align:left;color:#5f5f5f;font-size:12px;font-family:&quot;direction:ltr;box-sizing:border-box" valign="top"><p class="style1 text-center" style="text-align:center;margin:0;padding:0;color:#000;font-size:32px;font-family:&quot">Somebody wrote to you!</p></td></tr></tbody></table></div></td></tr><tr><td class="drow" style="background-color:#fff;box-sizing:border-box;font-size:0;text-align:center" valign="top" align="center"><div style="max-width:600px;display:inline-block;vertical-align:top;width:100%"><table class="edcontent" style="border-collapse:collapse;width:100%" cellspacing="0" border="0"><tbody><tr><td class="edtext" style="padding:10px 20px;text-align:left;color:#5f5f5f;font-size:12px;font-family:&quot;direction:ltr;box-sizing:border-box" valign="top"><p class="style2" style="margin:0;padding:0;color:#000;font-size:22px;font-family:&quot">Name:</p><p style="margin:0;padding:0;font-size:14px;font-family:&quot">${t_name}</p></td></tr></tbody></table></div></td></tr><tr><td class="drow" style="background-color:#fff;box-sizing:border-box;font-size:0;text-align:center" valign="top" align="center"><div style="max-width:600px;display:inline-block;vertical-align:top;width:100%"><table class="edcontent" style="border-collapse:collapse;width:100%" cellspacing="0" border="0"><tbody><tr><td class="edtext" style="padding:10px 20px;text-align:left;color:#5f5f5f;font-size:12px;font-family:&quot;direction:ltr;box-sizing:border-box" valign="top"><p class="style2" style="margin:0;padding:0;color:#000;font-size:22px;font-family:&quot">Email:</p><p style="margin:0;padding:0;font-size:14px;font-family:&quot">${t_email}</p></td></tr></tbody></table></div></td></tr><tr><td class="drow" style="background-color:#fff;box-sizing:border-box;font-size:0;text-align:center" valign="top" align="center"><div style="max-width:600px;display:inline-block;vertical-align:top;width:100%"><table class="edcontent" style="border-collapse:collapse;width:100%" cellspacing="0" border="0"><tbody><tr><td class="edtext" style="padding:10px 20px;text-align:left;color:#5f5f5f;font-size:12px;font-family:&quot;direction:ltr;box-sizing:border-box" valign="top"><p class="style2" style="margin:0;padding:0;color:#000;font-size:22px;font-family:&quot">Message:</p><pre style="margin:0;padding:0;font-size:14px;font-family:&quot">${t_message}</pre></td></tr></tbody></table></div></td></tr></tbody></table></td></tr></tbody></table></body>`,
               disableFileAccess: true,
             })
 
             console.log('SMTP response:', info)
+
             res.status(200).json({ success: true, data: "Thank you for your message, I will get back to you soon!" })
             resolve()
+
           } catch (err) {
             console.error(err)
             res.status(200).json({ success: false, data: "Some error occured!" })
             resolve()
           }
+        } else {
+          return res.status(401).json({ success: false, data: "Unauthorised!" })
         }
       })
     })
